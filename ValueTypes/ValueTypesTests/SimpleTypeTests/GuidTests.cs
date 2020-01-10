d5cffa912bd6b305a93c86e96c0b976df3047c68
Ryan Harris <trharris@auw.com>
2020-01-10 12:18:49 -0800

    Added an implicit cast from Guid plus supporting tests

---
+++ ValueTypes/ValueTypesTests/SimpleTypeTests/GuidTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ValueTypes;
+
+namespace ValueTypesTests.SimpleTypeTests
+{
+    [TestClass]
+    public class GuidTests : AbstractValueTypeTests<Guid>
+    {
+        protected override ValueBase GetOtherValue() => new Guid("{9BC8C864-E196-4162-8C60-D0AE98D2B781}");
+        protected override ValueBase GetSampleValue1() => new Guid("{0B6642DF-7E87-46B5-AA67-699941479ACF}");
+        protected override ValueBase GetSampleValue2() => new Guid("{0B6642DF-7E87-46B5-AA67-699941479ACF}");
+    }
+}
